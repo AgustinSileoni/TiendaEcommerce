d3cbcde4d9f206992e74bd1b2f69d43bb1752c03
Agustin Sileoni <agustinsileoni98@gmail.com>
2024-04-17 21:08:07 -0300

    Project initialized

---
+++ Project/src/test/java/com/agustin/sileoni/TiendaEcommerce/TiendaEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agustin.sileoni.TiendaEcommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaEcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
