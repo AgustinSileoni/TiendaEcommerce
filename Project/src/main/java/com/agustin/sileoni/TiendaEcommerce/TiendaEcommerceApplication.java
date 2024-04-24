61f808090e9ab2c74918d145d70df105d59f1288
Agustin Sileoni <agustinsileoni98@gmail.com>
2024-04-23 22:47:05 -0300

    Realizada la conexion con la base de datos mysql

--- Project/src/main/java/com/agustin/sileoni/TiendaEcommerce/TiendaEcommerceApplication.java
+++ Project/src/main/java/com/agustin/sileoni/TiendaEcommerce/TiendaEcommerceApplication.java
@@ -2,9 +2,8 @@ package com.agustin.sileoni.TiendaEcommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
 public class TiendaEcommerceApplication {
 
 	public static void main(String[] args) {
