b4297105cf4fe5355103c35141cfe7fecb11f677
AgustinSileoni <agustinsileoni98@gmail.com>
2024-05-23 00:04:31 -0300

    Agregada la funcionalidad de poner cosas en el carrito y que nos muestre el precio total y subtotal

--- Project/src/main/java/com/agustin/sileoni/TiendaEcommerce/controller/HomeController.java
+++ Project/src/main/java/com/agustin/sileoni/TiendaEcommerce/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.agustin.sileoni.TiendaEcommerce.controller;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 import org.slf4j.Logger;
@@ -8,7 +10,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.agustin.sileoni.TiendaEcommerce.model.DetalleOrden;
+import com.agustin.sileoni.TiendaEcommerce.model.Orden;
 import com.agustin.sileoni.TiendaEcommerce.model.Producto;
 import com.agustin.sileoni.TiendaEcommerce.service.ProductoService;
 
@@ -26,6 +31,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class HomeController {
 
     private final Logger log = LoggerFactory.getLogger(HomeController.class);
+
+    List<DetalleOrden> detallesOrden = new ArrayList<DetalleOrden>();
+    
+    Orden orden = new Orden();
+
     @Autowired
     private ProductoService productoService;
 
@@ -36,7 +46,7 @@ public class HomeController {
     }
 
     @GetMapping("productoHome/{id}")
-    public String productoHome( @PathVariable("id") Integer id, Model model) {
+    public String productoHome(@PathVariable("id") Integer id, Model model) {
         log.info("Id producto enviado como parametro {}", id);
         Optional<Producto> productoOptional = productoService.get(id);
         Producto producto = productoOptional.get();
@@ -45,7 +55,25 @@ public class HomeController {
     }
     
     @PostMapping("/cart")
-    public String addCart() {
+    public String addCart(@RequestParam("id") Integer id, @RequestParam("cantidad") Integer cantidad, Model model) {
+        DetalleOrden detalleOrden = new DetalleOrden();
+        Producto producto = productoService.get(id).get();
+        double sumaTotal = 0;
+
+        detalleOrden.setCantidad(cantidad);
+        detalleOrden.setNombre(producto.getNombre());
+        detalleOrden.setPrecio(producto.getPrecio());
+        detalleOrden.setTotal(producto.getPrecio() * cantidad);
+        detalleOrden.setProducto(producto);
+        
+        detallesOrden.add(detalleOrden);
+
+        sumaTotal = detallesOrden.stream().mapToDouble(dt-> dt.getTotal()).sum();
+        orden.setTotal(sumaTotal);
+
+        model.addAttribute("cart", detalleOrden);
+        model.addAttribute("orden", orden);
+
         return "usuario/carrito";
     }
     
