b558f9fd0efdf91201c017cc1575dc4638b8aaed
AgustinSileoni <agustinsileoni98@gmail.com>
2024-05-25 18:28:40 -0300

    Solucinado el hecho de que al agregar al carrit un producto que ya estaba se agregaba como uno nuevo, ahora le suma la cantidad al existente. Se puede acceder al carrito desde opcion del header

--- Project/src/main/java/com/agustin/sileoni/TiendaEcommerce/controller/HomeController.java
+++ Project/src/main/java/com/agustin/sileoni/TiendaEcommerce/controller/HomeController.java
@@ -20,7 +20,6 @@ import com.agustin.sileoni.TiendaEcommerce.service.ProductoService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 
@@ -32,7 +31,7 @@ public class HomeController {
 
     private final Logger log = LoggerFactory.getLogger(HomeController.class);
 
-    List<DetalleOrden> detallesOrden = new ArrayList<DetalleOrden>();
+    List<DetalleOrden> listaDetallesOrden = new ArrayList<DetalleOrden>();
     
     Orden orden = new Orden();
 
@@ -42,6 +41,8 @@ public class HomeController {
     @GetMapping("")
     public String home(Model model) {
         model.addAttribute("productos", productoService.findAll());
+        model.addAttribute("cart", listaDetallesOrden);
+        model.addAttribute("orden",orden);
         return "usuario/home";
     }
 
@@ -54,28 +55,80 @@ public class HomeController {
         return "usuario/productohome";
     }
     
-    @PostMapping("/cart")
+    @PostMapping("cart")
     public String addCart(@RequestParam("id") Integer id, @RequestParam("cantidad") Integer cantidad, Model model) {
-        DetalleOrden detalleOrden = new DetalleOrden();
-        Producto producto = productoService.get(id).get();
-        double sumaTotal = 0;
-
-        detalleOrden.setCantidad(cantidad);
-        detalleOrden.setNombre(producto.getNombre());
-        detalleOrden.setPrecio(producto.getPrecio());
-        detalleOrden.setTotal(producto.getPrecio() * cantidad);
-        detalleOrden.setProducto(producto);
         
-        detallesOrden.add(detalleOrden);
+        Producto producto = productoService.get(id).get();
+        Integer idProducto = producto.getIdProducto();
+        boolean ingresado = listaDetallesOrden.stream().anyMatch(p -> p.getProducto().getIdProducto() == idProducto);
 
-        sumaTotal = detallesOrden.stream().mapToDouble(dt-> dt.getTotal()).sum();
-        orden.setTotal(sumaTotal);
+        if(ingresado){
+            for (DetalleOrden detalleOrden: listaDetallesOrden){
+                if (detalleOrden.getProducto().getIdProducto()==id){
+                    detalleOrden.setCantidad(detalleOrden.getCantidad()+cantidad);
+                    detalleOrden.setTotal(detalleOrden.getTotal()+(cantidad)*detalleOrden.getPrecio());
+                }
+                else{
+                    
+                }
+            }
+        }
+        else{
+            DetalleOrden detalleOrden = new DetalleOrden();
+            double sumaTotal = 0;
 
-        model.addAttribute("cart", detalleOrden);
+            detalleOrden.setCantidad(cantidad);
+            detalleOrden.setNombre(producto.getNombre());
+            detalleOrden.setPrecio(producto.getPrecio());
+            detalleOrden.setTotal(producto.getPrecio() * cantidad);
+            detalleOrden.setProducto(producto);
+            
+            listaDetallesOrden.add(detalleOrden);
+            sumaTotal = listaDetallesOrden.stream().mapToDouble(dt-> dt.getTotal()).sum();
+            orden.setTotal(sumaTotal);
+            
+        }
+
+        model.addAttribute("cart", listaDetallesOrden);
         model.addAttribute("orden", orden);
+
+        log.info("Datos de la lista {}", listaDetallesOrden);
+        log.info("Datos de la orden",orden );
 
         return "usuario/carrito";
     }
+
+    @GetMapping("cart/delete/{id}")
+    public String deleteProductoCart(@PathVariable("id") Integer id, Model model) {
+        
+         List<DetalleOrden> ordenesNuevas =  new ArrayList<DetalleOrden>();
+
+        for(DetalleOrden detalleOrden:listaDetallesOrden){
+            if(detalleOrden.getProducto().getIdProducto()!=id){
+                ordenesNuevas.add(detalleOrden);
+            }
+        }
+
+        listaDetallesOrden = ordenesNuevas;
+        double sumaTotal = 0;
+        sumaTotal = listaDetallesOrden.stream().mapToDouble(dt->dt.getTotal()).sum();
+        orden.setTotal(sumaTotal);
+        model.addAttribute("cart", listaDetallesOrden);
+        model.addAttribute("orden",orden);
+
+        log.info("Datos de la lista {}", listaDetallesOrden);
+        log.info("Datos de la orden",orden );
+        return "/usuario/carrito";
+    }
+    
+    @GetMapping("/getCart")
+    public String getCart(Model model){
+        model.addAttribute("cart", listaDetallesOrden);
+        model.addAttribute("orden", orden);
+        return "/usuario/carrito";
+    }
     
     
+
+
 }
