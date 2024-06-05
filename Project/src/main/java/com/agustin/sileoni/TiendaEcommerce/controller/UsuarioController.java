bf058d69a132d15b311f8a1f0aa4da51cc3e3b0e
AgustinSileoni <agustinsileoni98@gmail.com>
2024-06-04 23:14:26 -0300

    Agregada la opcion de logout una vez se inicio sesion

--- Project/src/main/java/com/agustin/sileoni/TiendaEcommerce/controller/UsuarioController.java
+++ Project/src/main/java/com/agustin/sileoni/TiendaEcommerce/controller/UsuarioController.java
@@ -15,6 +15,8 @@ import jakarta.servlet.http.HttpSession;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 
@@ -73,6 +75,15 @@ public class UsuarioController {
 
         return "redirect:/usuario/login";
     }
+    
+
+    @GetMapping("/logout")
+    public String logout(HttpSession httpSession) {
+        httpSession.removeAttribute("idUsuario");
+
+        return "redirect:/usuario/login";
+    }
+    
     
     
 
